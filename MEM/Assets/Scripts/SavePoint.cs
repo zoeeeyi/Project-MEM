6a92748fec4d97c82acf3bd2a51f7fc90dc0d1d5
Zoey Tang <agentgt17@gmail.com>
2022-06-23 16:35:13 -0700

    Checkpoint

--- MEM/Assets/Scripts/SavePoint.cs
+++ MEM/Assets/Scripts/SavePoint.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class SavePoint : MonoBehaviour
 {
     SavePointController savePointController;
-    // Start is called before the first frame update
+    List<Collider2D> characters = new List<Collider2D>();
+    private Renderer rend;
+
     void Start()
     {
+        rend = GetComponent<Renderer>();
         savePointController = GameObject.Find("SavePointController").GetComponent<SavePointController>();
+    }
+
+    private void Update()
+    {
+        if (characters.Count == 2)
+        {
+            savePointController.lastSavePos = transform.position;
+            rend.material.color = Color.blue;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            savePointController.lastSavePos = transform.position;
+            if (!characters.Contains(collision)) characters.Add(collision);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (characters.Contains(collision)) characters.Remove(collision);
         }
     }
 }
