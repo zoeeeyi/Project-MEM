f819336b0d349477cc337ded50503052ca31cd54
Zoey Tang <agentgt17@gmail.com>
2022-07-13 14:30:45 -0700

    new Save Points

--- MEM/Assets/Scripts/SavePoint.cs
+++ MEM/Assets/Scripts/SavePoint.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SavePoint1 : MonoBehaviour
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class SavePoint : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool flipped;
+    float playerAngle = 0;
+
+    SavePointParent parent;
+    Renderer rend;
+    Color originalColor;
+
+    bool used = false;
+
     void Start()
     {
-        
+        if (flipped) playerAngle = 180;
+        rend = GetComponent<Renderer>();
+        originalColor = rend.material.color;
+        parent = GetComponentInParent<SavePointParent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (used) return;
+
+        if (parent.used)
+        {
+            Color newColor = Color.green;
+            rend.material.color = newColor;
+            used = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") 
+            && collision.transform.localEulerAngles.z == playerAngle 
+            && !parent.used)
+        {
+            parent.playerReached += 1;
+            Color newColor = Color.green;
+            newColor.a = 0.3f;
+            rend.material.color = newColor;
+            //if (!parent.characters.Contains(collision)) parent.characters.Add(collision);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")
+            && collision.transform.localEulerAngles.z == playerAngle
+            && !parent.used)
+        {
+            parent.playerReached -= 1;
+            rend.material.color = originalColor;
+            //if (parent.characters.Contains(collision)) parent.characters.Remove(collision);
+        }
     }
 }
