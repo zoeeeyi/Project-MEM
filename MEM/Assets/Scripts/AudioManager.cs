4000fe6366f3955f58f9732bc33bb0dc6c1d7663
Zoey Tang <agentgt17@gmail.com>
2022-08-05 15:00:06 -0700

    updates

--- MEM/Assets/Scripts/AudioManager.cs
+++ MEM/Assets/Scripts/AudioManager.cs
@@ -5,31 +5,42 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
+    AudioSource temp;
     List<AudioSource> audioSourceList = new List<AudioSource>();
-    Dictionary<string, AudioClip> AudioClipsDic = new Dictionary<string, AudioClip>();
+    Dictionary<string, AudioClips> AudioClipsDic = new Dictionary<string, AudioClips>();
     
     public List<AudioClips> AudioClipsList;
 
     void Start()
     {
+        temp = gameObject.AddComponent<AudioSource>();
+
         for (int i = 0; i < AudioClipsList.Count; i++)
         {
-            AudioClipsDic.Add(AudioClipsList[i].clipName, AudioClipsList[i].clip);
+            AudioClipsDic.Add(AudioClipsList[i].clipName, AudioClipsList[i]);
         }
     }
 
     void LateUpdate()
     {
-        foreach (AudioSource i in audioSourceList)
+        for (int i = 0; i < audioSourceList.Count; i++)
         {
-            if (!i.isPlaying) audioSourceList.Remove(i);
+            if (!audioSourceList[i].isPlaying)
+            {
+                AudioSource deleteThis = audioSourceList[i];
+                audioSourceList[i] = temp;
+                Destroy(deleteThis);
+                audioSourceList.Remove(audioSourceList[i]);
+            }
         }
     }
 
     public void playAudioClip(string name)
     {
         AudioSource new_audioSource = gameObject.AddComponent<AudioSource>();
-        new_audioSource.clip = AudioClipsDic[name];
+        new_audioSource.clip = AudioClipsDic[name].clip;
+        new_audioSource.volume = AudioClipsDic[name].volume;
+        new_audioSource.loop = AudioClipsDic[name].loop;
         audioSourceList.Add(new_audioSource);
         audioSourceList[audioSourceList.Count - 1].Play();
     }
@@ -40,4 +51,7 @@ public class AudioClips
 {
     public string clipName;
     public AudioClip clip;
+    [Range(0, 1)]
+    public float volume;
+    public bool loop;
 }
